9d078ac69150d30dc81c2284be7c6934bcbf0174
IrinaJ <irina.ul87@gmail.com>
2023-01-10 11:45:58 -0500

    refactored

--- Cards/cards.js
+++ Cards/cards.js
@@ -3,28 +3,47 @@ const div = document.querySelector('div');
 const img = document.querySelector('img');
 
 
-window.addEventListener("DOMContentLoaded", function() {
+// window.addEventListener("DOMContentLoaded", function() {
+//     btn.style.display = "block";
+//     axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+//         .then(res=> {
+//            deck_id = res.data.deck_id; 
+//            return deck_id;
+//         })
+//         .then(res=> {
+//             btn.addEventListener('click', function() {
+//                 axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
+//                 .then(res=> {
+//                     let num = Math.floor(Math.random()*45 + 1);
+//                     num *= Math.round(Math.random()) ? 1 : -1;
+//                     let img = document.createElement('img');
+//                     img.src = res.data.cards['0'].image;
+//                     img.style.rotate = `${num}deg`;
+//                     div.append(img);
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                     div.innerHTML = '<h1 style="text-align:center;">No more cards</h1>';
+//                 });
+//             });
+//         });
+// });
+
+// -------------------Async/await----------------------------------
+
+window.addEventListener("DOMContentLoaded", async function() {
     btn.style.display = "block";
-    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-        .then(res=> {
-           deck_id = res.data.deck_id; 
-           return deck_id;
-        })
-        .then(res=> {
-            btn.addEventListener('click', function() {
-                axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-                .then(res=> {
-                    let num = Math.floor(Math.random()*45 + 1);
-                    num *= Math.round(Math.random()) ? 1 : -1;
-                    let img = document.createElement('img');
-                    img.src = res.data.cards['0'].image;
-                    img.style.rotate = `${num}deg`;
-                    div.append(img);
-                })
-                .catch(err => {
-                    console.log(err);
-                    div.innerHTML = '<h1 style="text-align:center;">No more cards</h1>';
-                });
-            });
-        });
+    let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+    deck_id = res.data.deck_id; 
+    
+    btn.addEventListener('click', async function() {
+        let res =await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
+        let num = Math.floor(Math.random()*45 + 1);
+        num *= Math.round(Math.random()) ? 1 : -1;
+        let img = document.createElement('img');
+        img.src = res.data.cards['0'].image;
+        img.style.rotate = `${num}deg`;
+        div.append(img);
+    });
 });
+    
--- Numbers/numbers.js
+++ Numbers/numbers.js
@@ -1,7 +1,7 @@
 const mult = document.querySelector('#multiple');
 const fav = document.querySelector('#favorite');
 
-// 1.
+// ----------------- 1.
 // function numberFacts(favoriteNumber) {
 //     axios.get(`http://numbersapi.com/${favoriteNumber}?json`)
 //         .then((res => {
@@ -10,31 +10,73 @@ const fav = document.querySelector('#favorite');
 //         .catch(err => console.log(err));
 // }
 
-// 2.
-axios
-    .get('http://numbersapi.com/2,23,7,12')
-    .then(res => {
-        for (let num in res.data) {
-           let li = document.createElement('li');
-           li.innerText = res.data[num];
-           mult.append(li);
-        }
-    })
-    .catch(err => console.log(err));
+// ----------------- 1. async/await
+// async function numberFacts(favoriteNumber) {
+//     try {
+//         let res = await axios.get(`http://numbersapi.com/${favoriteNumber}?json`);
+//         console.log(res.data.text);
+//     } catch {
+//        console.log("OOPS!");
+//     }
+// }
 
-// 3.
-let fourFacts = [];
-for (let i = 1; i < 5; i++) {
-    fourFacts.push(
-        axios.get('http://numbersapi.com/5?json')
-    );
-}
-Promise.all(fourFacts)
-    .then(facts => {
-        facts.forEach(fact => {
+// ---------------- 2.
+// axios
+//     .get('http://numbersapi.com/2,23,7,12')
+//     .then(res => {
+//         for (let num in res.data) {
+//            let li = document.createElement('li');
+//            li.innerText = res.data[num];
+//            mult.append(li);
+//         }
+//     })
+//     .catch(err => console.log(err));
+
+// ---------------- 2. async/await
+async function multipleNumbers() {
+    try {
+        let res = await axios.get('http://numbersapi.com/2,23,7,12');
+        for (let num in res.data) {
             let li = document.createElement('li');
-            li.innerText = fact.data.text;
-            fav.append(li);
-        });
-    })
-    .catch(err => console.log(err));
+            li.innerText = res.data[num];
+            mult.append(li);
+        }
+    } catch {
+        console.log('OOOPS!');
+    }
+}
+multipleNumbers();
+
+// ---------------3.
+// let fourFacts = [];
+// for (let i = 1; i < 5; i++) {
+//     fourFacts.push(
+//         axios.get('http://numbersapi.com/5?json')
+//     );
+// }
+// Promise.all(fourFacts)
+//     .then(facts => {
+//         facts.forEach(fact => {
+//             let li = document.createElement('li');
+//             li.innerText = fact.data.text;
+//             fav.append(li);
+//         });
+//     })
+//     .catch(err => console.log(err));
+
+    // ------------ 3. Async/await
+
+async function fourFacts(number) {
+    let url = `http://numbersapi.com/${number}?json`;
+    let facts = [];
+    for (let i=0; i<4; i++) {
+        facts.push(await axios.get(url));
+    }
+    for (let fact of facts) {
+        let li = document.createElement('li');
+        li.innerText = fact.data.text;
+        fav.append(li);
+    }
+}
+
+fourFacts(4);
